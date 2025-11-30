1ce242fb49b8b5c2f3d5e713b61cabbd729f33d8
jeremt <taboada.jeremie@gmail.com>
2025-12-01 00:20:42 +0100

    wip: sql file selection

--- src/lib/scripts/SqlScriptMenu.svelte
+++ src/lib/scripts/SqlScriptMenu.svelte
@@ -1,10 +1,51 @@
 <script lang="ts">
+    import CheckIcon from "$lib/icons/CheckIcon.svelte";
     import ClearIcon from "$lib/icons/ClearIcon.svelte";
+    import EnterIcon from "$lib/icons/EnterIcon.svelte";
+    import JsonFileIcon from "$lib/icons/JsonFileIcon.svelte";
     import PlayIcon from "$lib/icons/PlayIcon.svelte";
+    import Dialog from "$lib/widgets/Dialog.svelte";
     import {getScriptsContext} from "./scriptsContext.svelte";
+    import ItemSelect from "$lib/widgets/ItemSelect.svelte";
 
     const scripts = getScriptsContext();
+
+    type FileRow = {name: string; path: string};
+    let currentFile = $state<FileRow>({name: "test.sql", path: "~/Downloads"});
+    const files: FileRow[] = [
+        {name: "test.sql", path: "~/Downloads"},
+        {name: "lol.sql", path: "~/Projects/lol/sql"},
+    ];
+
+    let isFileSelectOpen = $state(false);
+    const itemToString = (item: FileRow) => `${item.name} ${item.path}`;
+    const onselect = (item: FileRow) => {
+        currentFile = item;
+        isFileSelectOpen = false;
+        console.log("TODO: select ", item);
+    };
 </script>
+
+<!-- <button class="btn ghost" title="⌘P" onclick={() => (isFileSelectOpen = true)} disabled={false}>
+    {currentFile.name}
+</button> -->
+
+<Dialog isOpen={isFileSelectOpen} onrequestclose={() => (isFileSelectOpen = false)} --padding="1rem">
+    <ItemSelect items={files} {itemToString} {onselect}>
+        {#snippet renderItem(item, index, selectedIndex, highlights)}
+            {#if currentFile && itemToString(currentFile) === itemToString(item)}
+                <CheckIcon --size="1.2rem" />
+            {:else}
+                <JsonFileIcon --size="1.2rem" />
+            {/if}
+            {#if highlights}<span class="search-result">{@html highlights}</span>{:else}{item.name}{/if}
+            <span class="font-normal text-xs text-fg-1 text-start grow overflow-hidden text-ellipsis">{item.path}</span>
+            {#if index === selectedIndex}
+                <EnterIcon />
+            {/if}
+        {/snippet}
+    </ItemSelect>
+</Dialog>
 
 <button
     class="btn ghost ml-auto"
@@ -17,3 +58,12 @@
 <button class="btn" onclick={scripts.run} title="⌘↵"
     ><PlayIcon --size="1.2rem" /> Run {scripts.currentSelection ? "selection" : "file"}</button
 >
+
+<style>
+    .search-result {
+        color: var(--color-fg-2);
+        :global(b) {
+            color: var(--color-fg);
+        }
+    }
+</style>
---
+++ src/lib/widgets/ItemSelect.svelte
@@ -0,0 +1,115 @@
+<script lang="ts" generics="ItemType">
+    import {fuzzySearchWithHighlights, renderHighlightedMatch} from "$lib/helpers/fuzzySearch";
+    import type {Snippet} from "svelte";
+
+    type Props = {
+        items: ItemType[];
+        itemToString: (item: ItemType) => string;
+        onselect: (item: ItemType) => void;
+        renderItem: Snippet<[item: ItemType, index: number, selectedIndex: number, highlights?: string]>;
+        placeholder?: string;
+        noResult?: string;
+    };
+
+    let {
+        items,
+        itemToString,
+        onselect,
+        renderItem,
+        placeholder = "Filter items",
+        noResult = "No result found",
+    }: Props = $props();
+
+    let searchText = $state("");
+    let searchResult = $derived(
+        fuzzySearchWithHighlights(searchText, items.map(itemToString)).map(({item, ranges}) => ({
+            text: item,
+            html: renderHighlightedMatch(item, ranges),
+        }))
+    );
+
+    let selectedIndex = $state(0);
+    let buttonRefs = $state<HTMLButtonElement[]>([]);
+
+    $effect(() => {
+        if (buttonRefs[selectedIndex]) {
+            buttonRefs[selectedIndex].scrollIntoView({
+                behavior: "instant",
+                block: "nearest",
+            });
+        }
+    });
+
+    const handleKeys = (event: KeyboardEvent) => {
+        if (event.key === "Enter") {
+            const selectedItem =
+                searchText === ""
+                    ? items[selectedIndex]
+                    : items.find((item) => itemToString(item) === searchResult[selectedIndex].text);
+            if (selectedItem) {
+                onselect(selectedItem);
+                searchText = "";
+            }
+        } else if (event.key === "ArrowUp") {
+            selectedIndex =
+                selectedIndex === 0 ? (searchText === "" ? items.length : searchResult.length) - 1 : selectedIndex - 1;
+        } else if (
+            event.key === "ArrowDown" &&
+            selectedIndex + 1 < (searchText === "" ? items.length : searchResult.length)
+        ) {
+            selectedIndex += 1;
+        } else {
+            selectedIndex = 0;
+        }
+    };
+</script>
+
+<div class="flex flex-col gap-2 w-2xl overflow-hidden">
+    <input type="text" bind:value={searchText} onkeydown={handleKeys} autocorrect="off" {placeholder} />
+    <div class="flex flex-col gap-2 overflow-auto h-80 py-2">
+        {#if searchText === ""}
+            {#each items as item, i}
+                <button
+                    bind:this={buttonRefs[i]}
+                    class="btn ghost justify-start!"
+                    class:selected-table={i === selectedIndex}
+                    onclick={() => {
+                        onselect(item);
+                    }}
+                >
+                    {@render renderItem(item, i, selectedIndex)}
+                </button>
+            {/each}
+        {:else}
+            {#each searchResult as { text, html }, i}
+                {@const item = items.find((item) => itemToString(item) === text)}
+                {#if item}
+                    <button
+                        bind:this={buttonRefs[i]}
+                        class="btn ghost justify-start!"
+                        class:selected-table={i === selectedIndex}
+                        onclick={() => {
+                            if (item) {
+                                onselect(item);
+                                searchText = "";
+                            }
+                        }}
+                    >
+                        {@render renderItem(item, i, selectedIndex, html)}
+                    </button>
+                {/if}
+            {:else}
+                <p class="text-sm text-center text-fg-2">{noResult}</p>
+            {/each}
+        {/if}
+    </div>
+</div>
+
+<style>
+    input[type="text"] {
+        background-color: transparent;
+    }
+    .selected-table {
+        background-color: var(--color-bg-1) !important;
+    }
+</style>
