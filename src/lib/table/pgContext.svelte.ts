aab60b23ff8dd7e07a2f39066f12bd62f9bc9d96
jeremt <taboada.jeremie@gmail.com>
2026-01-26 18:49:38 +0100

    feat: add cascade when trucating table

--- src/lib/table/pgContext.svelte.ts
+++ src/lib/table/pgContext.svelte.ts
@@ -308,7 +308,7 @@ WHERE ${pk.column_name} = ANY(ARRAY[${this.selectedRows
     };
 
     truncateTable = async () => {
-        await this.rawQuery(`TRUNCATE ${this.fullName} RESTART IDENTITY`, {throwError: false});
+        await this.rawQuery(`TRUNCATE ${this.fullName} RESTART IDENTITY CASCADE`, {throwError: false});
         this.filters.where = "";
     };
 
