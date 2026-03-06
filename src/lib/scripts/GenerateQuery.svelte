b4daa695466f7e7209b3cb5ae4e454328b579bf7
jeremt <taboada.jeremie@gmail.com>
2026-03-06 22:50:22 +0100

    feat: handle ai settings

--- src/lib/scripts/GenerateQuery.svelte
+++ src/lib/scripts/GenerateQuery.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+    import CogIcon from "$lib/icons/CogIcon.svelte";
+    import CrossIcon from "$lib/icons/CrossIcon.svelte";
     import SparklesIcon from "$lib/icons/SparklesIcon.svelte";
+    import TrashIcon from "$lib/icons/TrashIcon.svelte";
     import MultilinesInput from "$lib/widgets/MultilinesInput.svelte";
     import PasswordInput from "$lib/widgets/PasswordInput.svelte";
     import Select from "$lib/widgets/Select.svelte";
@@ -7,24 +10,62 @@
 
     const query_generator = get_query_generator_context();
     let api_key = $state("");
+    let mode = $state<"chat" | "settings">("chat");
 </script>
 
 <div class="flex flex-col gap-4 items-start h-full">
     {#if query_generator.api_key === undefined}
-        <label class="flex flex-col gap-4">
-            <span class="text-xs text-fg-1">
-                You must provide your OpenAI api key to generate queries (it only be saved locally on your computer and
-                never shared)
-            </span>
-            <PasswordInput placeholder="sk-proj-***" bind:value={api_key} />
-        </label>
-        <button
-            class="btn"
-            type="button"
-            onclick={(e) => ((e.preventDefault(), (query_generator.api_key = api_key)), query_generator.save_api_key())}
-            >Save api key</button
-        >
-    {:else}
+        <div class="flex flex-col p-4 gap-4">
+            <label class="flex flex-col gap-4">
+                <span class="text-xs text-fg-1">
+                    You must provide your OpenAI api key to generate queries (it only be saved locally on your computer
+                    and never shared)
+                </span>
+                <PasswordInput placeholder="sk-proj-***" bind:value={api_key} />
+            </label>
+            <button
+                class="btn self-start"
+                type="button"
+                onclick={(e) => {
+                    e.preventDefault();
+                    query_generator.api_key = api_key;
+                    query_generator.save_api_key();
+                    mode = "chat";
+                }}>Save api key</button
+            >
+        </div>
+    {:else if mode === "settings"}
+        <div class="flex flex-col p-4 gap-4 w-full">
+            <div class="flex w-full gap-2 items-center">
+                <button class="btn ghost icon" onclick={() => (mode = "chat")}><CrossIcon --size="1.2rem" /></button>
+                <div class="font-bold">AI Settings</div>
+            </div>
+            <label class="flex flex-col gap-4">
+                <span class="text-xs text-fg-1">Update your api key </span>
+                <PasswordInput placeholder="sk-proj-***" bind:value={api_key} />
+            </label>
+            <div class="flex justify-between w-full">
+                <button
+                    class="btn"
+                    type="button"
+                    onclick={(e) => {
+                        e.preventDefault();
+                        query_generator.api_key = api_key;
+                        query_generator.save_api_key();
+                        mode = "chat";
+                    }}>Save api key</button
+                >
+                <button
+                    class="btn error"
+                    onclick={(e) => {
+                        e.preventDefault();
+                        query_generator.api_key = undefined;
+                        query_generator.save_api_key();
+                    }}><TrashIcon --size="1rem" /> Clear api key</button
+                >
+            </div>
+        </div>
+    {:else if mode === "chat"}
         <div class="w-full overflow-auto grow">
             <!-- Tool call log -->
             {#if query_generator.tool_log.length > 0}
@@ -57,12 +98,15 @@
                 bind:value={query_generator.query_prompt}
                 placeholder="Describe your SQL query…"
             />
-            <div class="flex justify-between w-full">
+            <div class="flex gap-2 w-full">
+                <button class="btn ghost icon" aria-label="Settings" onclick={() => (mode = "settings")}
+                    ><CogIcon --size="1rem" /></button
+                >
                 <Select bind:value={query_generator.model} onchange={() => query_generator.save_model()}>
                     <option value="gpt-5">gpt-5 </option>
                     <option value="gpt-5-mini">gpt-5-mini</option>
                 </Select>
-                <button class="btn" onclick={query_generator.generate} disabled={query_generator.is_generating}>
+                <button class="btn ms-auto" onclick={query_generator.generate} disabled={query_generator.is_generating}>
                     <SparklesIcon --size="1rem" />
                     {query_generator.is_generating ? "Generating..." : "Generate"}
                 </button>
